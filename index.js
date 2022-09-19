9ce77677e33ab7f35d78871daea07e9404c1ecb7
samanthamarberger <slatham04@gmail.com>
2022-09-19 12:11:32 -0600

    commit tree 49a563e90e148fea1d0046a865a524f155d25d78

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  else if (distance < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (distance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let isGo;
+  city === 'NYC' ? (isGo = "Ok, sounds good.") : (isGo = "No go.");
+
+  return isGo;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
